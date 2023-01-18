c9622971834d06eacf906f14fde27950c0c5bff7
Ivi Alex Franco Silva <49818959+ivialex@users.noreply.github.com>
2023-01-17 22:08:28 -0300

    Update build-dev-environment.groovy

--- pipelines/build-dev-environment.groovy
+++ pipelines/build-dev-environment.groovy
@@ -35,7 +35,7 @@ pipeline {
                     """
 
                     sh """
-                    sudo docker build pipelines/ -t $params.ENVIRONMENT_NAME:latest
+                    docker build pipelines/ -t $params.ENVIRONMENT_NAME:latest
                     """
 
                 }else{
@@ -51,11 +51,11 @@ pipeline {
                 def dateTime = (sh(script: "date +%Y%m%d%H%M%S", returnStdout: true).trim())
                 def containerName = "${params.ENVIRONMENT_NAME}_${dateTime}"
                 sh """
-                sudo docker run -itd --name ${containerName} --rm -e MYSQL_ROOT_PASSWORD=$params.MYSQL_PASSWORD -p $params.MYSQL_PORT:3306 $params.ENVIRONMENT_NAME:latest
+                docker run -itd --name ${containerName} --rm -e MYSQL_ROOT_PASSWORD=$params.MYSQL_PASSWORD -p $params.MYSQL_PORT:3306 $params.ENVIRONMENT_NAME:latest
                 """
 
                 sh """
-                sudo docker exec ${containerName} /bin/bash -c 'mysql --user="root" --password="$params.MYSQL_PASSWORD" < /scripts/create_developer.sql'
+                docker exec ${containerName} /bin/bash -c 'mysql --user="root" --password="$params.MYSQL_PASSWORD" < /scripts/create_developer.sql'
                 """
 
                 echo "Docker container created: $containerName"
